3237a52e000a587373868938e2cab6edb212c12e
Miguel Coelho <miname>
2025-02-20 17:32:42 +0000

    started button component

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,3 +1,24 @@
-export function Button() {
-  return <button>Tessadfasldkfhjt</button>;
+import classNames from "classnames";
+import { DetailedHTMLProps } from "react";
+import styles from "./button.module.css";
+
+export type ButtonProps = DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
+  variant?: "primary" | "secondary" | "tertiary";
+};
+export function Button(props: ButtonProps) {
+  const { children, className, variant = "tertiary", ...rest } = props;
+
+  return (
+    <button
+      className={classNames(className, "border", {
+        [styles.button_contained]: variant === "primary",
+      })}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
